c3e0789bc8d26fce09c1f08f1f5e82f81427df79
knair99 <karunakaran.prasad@gmail.com>
2018-03-31 00:49:56 -0700

    first commit

---
+++ Dental Dosage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dental Dosage
+//
+//  Created by Karunakaran Prasad on 3/31/18.
+//  Copyright © 2018 BayMonkeys. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
