92822ea7f3a938c830d58c006a4a05d81959859f
knair99 <karunakaran.prasad@gmail.com>
2018-04-12 01:55:26 -0700

    Dashboard - Added a table view with prototype cell for generic drug types

---
+++ Dental Dosage/Controllers/DashboardTableViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  DashboardTableViewCell.swift
+//  Dental Dosage
+//
+//  Created by Karunakaran Prasad on 4/12/18.
+//  Copyright © 2018 BayMonkeys. All rights reserved.
+//
+
+import UIKit
+
+class DashboardTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var drugTypeLabel: UILabel!
+    @IBOutlet weak var drugTypeImageView: UIImageView!
+    
+    //Declare all custom methods here
+    func setCell(drugLabel: String, drugImage: String){
+        drugTypeLabel.text = drugLabel
+        drugTypeImageView.image = UIImage(named: drugImage)
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+
+}
--- Dental Dosage/Controllers/DashboardViewController.swift
+++ Dental Dosage/Controllers/DashboardViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class DashboardViewController: UIViewController {
+    
     //Declare all locals here
+    var settings : Settings?
+    var dashboardDummyArray : [[String]] = [["M1", "M2", "M3"], ["R1", "R2", "R3"],["F1", "F2", "F3"]]
+    var currentIndex : Int = 0
     
     //Declare all outlets here
     
@@ -20,19 +24,32 @@ class DashboardViewController: UIViewController {
     func GetInitialUserSettings(){
         let defaults = UserDefaults.standard
         if let savedSettings = defaults.object(forKey: "DefaultSettingsKey") as? Data {
-            let settings = NSKeyedUnarchiver.unarchiveObject(with: savedSettings) as! Settings
-            print("\(settings)")
+            settings = NSKeyedUnarchiver.unarchiveObject(with: savedSettings) as? Settings
         }
     }
     
     //Declare all overrrides here
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        currentIndex = 0
         //Get initial user settings
         GetInitialUserSettings()
     }
-    
+}
 
+extension DashboardViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dashboardDummyArray[currentIndex].count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let index = indexPath.item
+        let dashboardValues = dashboardDummyArray[currentIndex][index]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "DashboardTableViewCell") as! DashboardTableViewCell
+        cell.setCell(drugLabel: dashboardValues, drugImage: "dashboard_anasthetic")
+        return cell
+    }
+    
+    
 }
 
