48af3ae4fcb43ce51148cdcbfb0afcd1b9b9943c
knair99 <karunakaran.prasad@gmail.com>
2018-04-16 04:00:30 -0700

    Drug List: Dynamically read data into cell view via JSON

--- Dental Dosage/Controllers/DrugListViewController.swift
+++ Dental Dosage/Controllers/DrugListViewController.swift
@@ -12,17 +12,48 @@ class DrugListViewController: UIViewController {
 
     //Declare all locals here
     var drugTypeName : String = "Dental Dosage"
-    var drugArray : [[String:Any]]?
+    //Contains dictionaries of each drug relevant to current drug type
+    var drugArray : [[String:Any]] = []
+    //Store just the names of the drugs relevant to current drug type
+    var drugNamesArray : [String] = []
     
     //Declare all outlets here
+    @IBOutlet weak var drugListTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //Load drug info from the JSON data
         
-        for drug in drugArray! {
-            print ("\(String(describing: drug["name"]))")
+        //Get individual drug names into specific types array for cell view later
+        for drug in drugArray {
+            let name = drug["name"] as! String
+            drugNamesArray.append(name)
         }
     }
-
 }
+
+extension DrugListViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return drugArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let index = indexPath.item
+        let drug = drugArray[index]
+        let name = drug["name"] as! String
+        var percent = drug["specs"] as? String
+        var brand =  drug["brand"] as? String
+        if brand == nil {
+            brand = name
+        }
+        if percent ==  nil || percent!.count >= 25 {
+            percent = drug["method"] as? String
+        }
+        let cell = drugListTableView.dequeueReusableCell(withIdentifier: "drugTableViewCell") as! DrugTypeTableViewCell
+        cell.setCell(name: name, percent: percent!, brand: brand!)
+        return cell
+    }
+    
+    
+    
+}
+
---
+++ Dental Dosage/Controllers/DrugTypeTableViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  DrugTypeTableViewCell.swift
+//  Dental Dosage
+//
+//  Created by Karunakaran Prasad on 4/16/18.
+//  Copyright © 2018 BayMonkeys. All rights reserved.
+//
+
+import UIKit
+
+class DrugTypeTableViewCell: UITableViewCell {
+
+    //Declare all outlets here
+    @IBOutlet weak var drugNameLabel: UILabel!
+    @IBOutlet weak var drugPercentLabel: UILabel!
+    @IBOutlet weak var drugBrandLabel: UILabel!
+    
+    //Declare all custom methods here
+    func setCell(name:String, percent:String, brand:String){
+        
+        drugNameLabel.text = name
+        drugBrandLabel.text = brand
+        drugPercentLabel.text = percent
+    }
+    
+    //Declare all overrides here
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
